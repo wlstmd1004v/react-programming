1e3d75ad8ef318a5e3d86e5fdea8f6405cacc201
wlstmd1004v <wlstmd1004v@gmail.com>
2023-08-10 00:13:18 +0900

    ✨

---
+++ src/layout/RootLayout.jsx
@@ -0,0 +1,19 @@
+//# eslint-disable react/prop-types 
+
+import HeaderBar from "./HeaderBar";
+import FooterBar from './FooterBar';
+
+function RootLayout(props) {
+  
+  return (
+    <div>
+      <HeaderBar />
+      <main>
+        {props.children}
+      </main>
+      <FooterBar />
+    </div>
+  );
+}
+
+export default RootLayout;
