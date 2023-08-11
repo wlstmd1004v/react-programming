0965937b375ad8cec6a9453e12c3c66ed3bf3086
wlstmd1004v <wlstmd1004v@gmail.com>
2023-08-11 12:53:34 +0900

    추가

---
+++ src/pages/Practice/parts/Controller.jsx
@@ -0,0 +1,15 @@
+//! 필요 없어요
+//! import React from 'react'
+
+import GoToButton from "./GoToButton"
+
+function Controller() {
+    return (
+        <div role="group" className="buttonGroup">
+            <GoToButton direction="down" label="스크롤 다운" />
+            <GoToButton direction="up" label="스크롤 업" />
+        </div>
+    );
+}
+
+export default Controller;
