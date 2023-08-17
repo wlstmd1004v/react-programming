03675c815c72ed76b807b03ea569d56d506e2734
wlstmd1004v <wlstmd1004v@gmail.com>
2023-08-17 22:57:46 +0900

    🎨 2023-08-17

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/prop-types': 'off',
+    "no-unused-vars": "warn",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
